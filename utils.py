e2b2ed7fdc09547a1df1e65c87404e8c0cdfe8d9
amir <amirhossein.maleki1990@gmail.com>
2022-02-13 17:00:33 +0330

    to Categorical list of names of videos.

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import numpy as np
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
+from tensorflow.keras.utils import to_categorical
 
 
 def frame_show(ds, video_no=0, frame_no=0, with_segment=True):
@@ -40,3 +42,25 @@ def shape_extractor(ds):
     for example in tfds.as_numpy(indim_shape):
         return example['video']['frames'].shape[1:], \
                example['video']['segmentations'].shape[1:]
+
+
+def video_name_to_categorical(ds):
+    """
+
+    :param ds: ds is a nested datasets including VIDEOs
+               and corresponding SEGMENTATIONs besides
+               the methadata for each VIDEO.
+
+    :return: This function returns the video name array
+             and categorical list of the dataset for the
+             classification purpose
+
+    TODO: the davis dataset has one category for each VIDEO,
+          it needs to reimplemented for dataset more than one
+           object in the a video.
+
+    """
+    video_name = np.array([x['metadata']['video_name'].numpy().decode("utf-8") for x in ds])
+    # uniq = np.unique(video_name)
+    categorical = to_categorical(np.arange(0, 60, 1), dtype='int32')
+    return categorical, video_name
